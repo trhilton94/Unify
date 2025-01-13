61a8b53938e2c60425894bed10ac8328e87e0052
Trey Hilton <trhilton94@gmail.com>
2025-01-13 17:37:29 -0500

    Docker Stuff

--- frontend/src/components/General/ErrorPage.tsx
+++ frontend/src/components/General/ErrorPage.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Navbar from './NavBar';
 
 export default function ErrorPage() {
     return (
         <div className="ErrorPage">
-            <h1>404 Not Found</h1>
+            <Navbar />
+            <h3>404 Not Found</h3>
         </div>
     );
 }
